9b5f99606c05a31bb9f1633368cd0db60956fd07
kaitonggao <50878288+kaitonggao@users.noreply.github.com>
2019-09-09 11:45:48 -0400

    9/9/19

---
+++ app.js
@@ -0,0 +1,19 @@
+/////////Primitives 
+/* let numbers = 4; //let values can be changed, const can't
+numbers = 5;
+let numbersArrays = [1,2,3,4,5]
+console.log(numbers);
+
+let string = 'string';
+let strings = "strings";
+
+let boolean = true; //also false */
+
+////////////
+
+const firstName = 'Rob';
+const job = 'Student';
+const birthYear = 2002;
+let isMarried = false;
+
+console.log(`Hello my name is ${firstName}, I am a ${job}, I was born in ${birthYear} and am I married? ${isMarried}`);
