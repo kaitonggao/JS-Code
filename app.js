a5b1b5c894b45a4ed29d03c125c76d34c57e671d
kaitonggao <50878288+kaitonggao@users.noreply.github.com>
2019-09-11 11:42:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ Dejon: 92kg, 1.95 meters
 3. Create a boolean variable containing information about whether Rob has a higher BMI than Dejon.
 4. Print a string to the console containing the variable from step 3. (Something like "Is Rob's BMI higher than Dejon's? true").*/
 
-const massRob = 78;
+/*const massRob = 78;
 const massDejon = 92;
 const heightRob = 1.69;
 const heightDejon = 1.95;
@@ -51,3 +51,73 @@ console.log (`Dejon's BMI is ${BMIDejon}.`) ;
 let Dejonhigher = BMIDejon > BMIRob; //boolean example - a statement that is either true or false //
 
 console.log (`Dejon's BMI is higher than Rob's? ${Dejonhigher}.`) ;
+*/
+
+/*let firstName = 'Paul';
+let status = 'single';
+
+if (status === 'single'){
+    console.log(`${firstName} is single and ready to mingle.`);
+} else{
+    console.log(`Someone made a big mistake with ${firstName}`);
+}
+
+//triple equal means strict! o: //
+
+const massRob = 78;
+const massDejon = 92;
+const heightRob = 1.69;
+const heightDejon = 1.95;
+
+let BMIRob = massRob/(heightRob * heightRob);
+let BMIDejon = massDejon/(heightDejon * heightDejon);
+
+if(BMIDejon > BMIRob){
+    console.log('Dejon is heavier :V');
+} else {
+    console.log(`Rob is heavier :<`)
+}
+let firstName = 'Sarah';
+let age = 17;
+
+if(age<13) {
+    console.log(`${firstName} is a young girl`);
+} else if (age<20) {
+    console.log(`${firstName} is a teenage.. ugh`);
+} else if (age >=20 && age<30) {
+    console.log(`${firstName} is a young woman `);
+} else {
+    console.log ('She a boomer');
+}
+
+let firstName = 'Kai';
+let grade = 4;
+let freshman = 1;
+let sophmore = 2;
+let junior = 3;
+let senior = 4;
+
+if(grade>3) {
+    console.log(`${firstName} needs to worry about college :<`)
+}
+
+*/
+
+//Ternary Operator --> Cleaner way of writing an if/else statement 
+
+let firstName = 'Melkadzez';
+let age = 17;
+
+age >= 18 ? console.log(`${firstName} will drink alcohol.`) : console.log(`${firstName} will drink orange juice! `)
+
+//condition? exprIfTrue : exprIfFalse
+
+/////falsey values: null, 0 , undefined, '' , NaN
+///truthy values : Anything not above this line.
+
+let height = 22;
+if (height == '22') {
+    console.log (`Hey this is a type coercion!`);
+} else {
+    console.log (`No type coercion with triple ===`)
+}
