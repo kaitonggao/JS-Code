0ddbada887f77d2454211eb08790b481f75b6932
kaitonggao <50878288+kaitonggao@users.noreply.github.com>
2019-09-19 11:46:27 -0400

    9-19

--- 9-17-Objects.js
+++ 9-17-Objects.js
@@ -3,6 +3,9 @@
 
 //collects or states a bunch of info for something ex: a song! or... a pokemon!
 
+//this. You must state this.age because the variables you are referrring to are not stated before, it is stated in the {} and thus can only be called if you use this.
+// use if you need to "return" a variable
+
 const hero= {
     alias: 'Spiderman',
     firstName: 'Peter',
@@ -14,8 +17,8 @@ const hero= {
         city: 'NYC'
     },
     getBirthYear: function(){
-        return 2019 - this.age; //this is calling back
-    }
+        return 2019 - this.age; //this calls variables stated in your const/function
+        }
 }
 
 
@@ -27,3 +30,4 @@ superMan.alias ='Superman';
 superMan.firstName = 'Clark';
 
 console.log(superMan);
+
